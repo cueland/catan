d7b6a8bbf8c436105c23e90f6156402909e3245c
cueland <christian.ueland@gmail.com>
2019-08-06 03:12:39 +0200

    new SVG print method output to file

--- catan.R
+++ catan.R
@@ -10,19 +10,9 @@ library(gridSVG)
 source(here("calc_adj.R"))
 source(here("cart.R"))
 source(here("get_res.R"))
+source(here("get_corners.R"))
+source(here("svg_element.R"))
 
-
-# plot_polygon <- function(xx,yy,fill,stroke, strokew) {
-#   # create the string to draw an SVG polygon with colors and borders
-#   # check if xx and yy have same length
-#     if(length(xx) != length(yy)) {
-#       stop("XX and YY are different length vectors")
-#     }
-#   # check 
-#   
-#   #create array with xx and yy values
-#   # x <- paste0("<polygon class=\"{fill:#", counter
-# }
 
 id <- paste(c(-1,-1,2,0,0,4,-1,0,6), collapse=",")
 
@@ -96,7 +86,8 @@ corners <- cbind(corners, corners1)
 rm(corners1)
 
 colhx <- data.frame(res = c("Brick", "Ore", "Sheep", "Wheat", "Wood", "Desert"),
-                    hex = c("#945337", "#b8a2c7","#c7e0ca", "#fff9b9", "#7aa86b", "#cccccc"))
+                    hex = c("#945337", "#b8a2c7","#c7e0ca", "#fff9b9", "#7aa86b", "#cccccc"),
+                    stringsAsFactors = F)
 tiles$ord <- 1:nrow(tiles)
 colss <- merge(tiles, colhx, by = "res", all.x= T)
 colss <- colss[order(colss$ord),]
@@ -105,16 +96,60 @@ colss$stars <- sapply(colss$strength, function(x){paste0(rep("*",x), collapse=""
 
 colss$desc <- as.expression(paste(colss$res, colss$value, colss$stars, sep="\n"))
 xx <- lapply(colss, function(x){expression(paste(x$res, x$value, x$stars, sep="/n"))})
-x <- split(unlist(lapply(as.list(data.frame(t(tiles[,c("ax", "hor")]))), FUN = function(x) {cart_find(c(x, 0))})), rep(1:2,19))
 
-dat <- lapply(as.list(data.frame(t(tiles[,c("ax", "hor")]))), FUN = get_corners)
-dat <- split(c(dat, as.character(colss$hex)), rep(1:19,2))
+# generate the center x,y coordinates for each hex
+hex_coords <- matrix(unlist(lapply(as.list(data.frame(t(tiles[,c("ax", "hor")]))),
+                                   FUN = function(x) {cart_find(c(x, 0))})),
+                     ncol = 2, byrow = T)
+# transform the coordinates to account for SVG format
+hex_coords_SVG <- t(c(450, 450) + t(hex_coords) * c(100, -100))
 
+# generate the x,y coordinates of each corner for every tile
+corner_coords <- lapply(as.list(data.frame(t(tiles[,c("ax", "hor")]))), FUN = function(x) get_corners(x))
+# transform the coordinates to account for SVG format
+corner_coords_SVG <- lapply(corner_coords, FUN = function(x) {t(c(450, 450) + (t(x) * c(100, -100)))})
 
-plot(-5:5,-5:5, type='n', axes=F, xlab="", ylab="")
-lapply(dat, function(x){polygon(x[[1]]$xx, x[[1]]$yy, col = x[[2]], border = "black")})
-text(x[[1]], x[[2]], labels = lapply(colss$desc, function(x) paste0(x)))
-# symbols(corners$xx, corners$yy, circles = rep(1,length(corners$xx)))
+# ---------------------------------------| Create the SVG file |--------------------------------------------- #
 
-svglite(file = "Rplots.svg", width = 10, height = 8, bg = "white",
-        pointsize = 12)
+header <- c("<?xml version=\"1.0\" encoding=\"utf-8\"?>",
+            "<!-- Generator: Adobe Illustrator 23.0.4, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->",
+            "<svg version=\"1.1\" id=\"Layer_1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" x=\"0px\" y=\"0px\"",
+            "\tviewBox=\"0 0 900 900\" style=\"enable-background:new 0 0 900 900;\" xml:space=\"preserve\">")
+
+# generate the an array of strings formatted for SVG polygon points
+points <- lapply(
+  lapply(corner_coords_SVG, FUN = function(x) {
+    apply(x,1,FUN = function(y) {paste0(y,collapse = ",")} )
+  }
+  ),
+  FUN = function(z) {paste0(z, collapse = " ")})
+
+# create the polygon text
+pgons <- unlist(mapply(FUN = function(x,y) {
+  paste0("<polygon points = '",x, "' style = 'fill:",y,";stroke:#353535;stroke-width:2' />")
+}, points, colss$hex))
+
+# create the text in the center of the hexagons
+texxt <- unlist(mapply(FUN = function(x, y, res, val, sth) {
+  paste0("<text dominant-baseline='middle' text-anchor='middle' transform='matrix(1 0 0 1 ",x, " ", y, ")' ",
+         "style='font-family:\"AGaramondPro-Regular\";font-size:24px;'>\n<tspan x='0' y='-1em'>", res, "</tspan>",
+         "<tspan x='0' y='0em'>", val, "</tspan>",
+         "<tspan x='0' y='1em'>", paste0(rep("*", sth), collapse = ""), "</tspan>",
+         "\n</text>", sep = "")
+      }, hex_coords_SVG[,1], hex_coords_SVG[,2], tiles$res, tiles$value, tiles$strength))
+
+footer <- "</svg>"
+
+# write the SVG code to a file
+write(c(header,pgons,texxt, footer), file = "polygons.svg")
+
+# ---------------------------------------| Plot using R |--------------------------------------------- #
+
+dat <- split(c(corner_coords, colss$hex), rep(1:19,2))
+x <- split(c(hex_coords), rep(1:19,2))
+plot_size <- 4.5
+
+plot(-plot_size:plot_size,-plot_size:plot_size, type='n', axes=F, xlab="", ylab="")
+lapply(dat, function(x){polygon(x[[1]][,1], x[[1]][,2], col = x[[2]], border = "black")})
+
+text(hex_coords[,1], hex_coords[,2], labels = lapply(colss$desc, function(x) paste0(x)))
---
+++ svg_element.R
@@ -0,0 +1,18 @@
+#' Generate an SVG element (svg)
+#'
+#' This function creates an SVG item object
+#' @param tiles A dataframe with a list axial coordinates and their corresponding resources
+#' @param id A string with nine coordinates ax,hor,corner for the 3 tiles corresponing to one corner
+#' @param x A vector with three elements in the format c(ax, hor, corner).
+#' @keywords
+#' @export
+#' @examples
+#' get_res(tiles, id="0,-3,2,1,-2,4,0,-2,6")
+#' get_res(tiles, coords=c(1,-2,4))
+#' 
+ setClass("svg_element", slots = list(tag = "character")
+
+          
+svg_element <- function(type, attributes) {
+  x <- paste0("<",type,"")
+}
